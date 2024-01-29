b7d7bbab66a18233ecfab1ac47e939052e6c3a54
RISHAB GUPTA <rishabkumar0703@gmail.com>
2024-01-29 20:41:18 +0530

    episode 2

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 /* <div id="parent">
     <div id ="child1">
         <h1> I am h1 tag</h1>
@@ -16,13 +19,13 @@
 
 const parent = React.createElement("div", { id:"parent" }, [
     React.createElement("div", { id:"child1" }, [
-        React.createElement("h1", {}, "I am an h1 tag"),
+        React.createElement("h1", {}, "I am an h1 tag 🚀"),
         React.createElement("h2", {}, "I am h2 tag")
     ]),
 
     React.createElement("div", { id:"child2" }, [
         React.createElement("h1", {}, "I am an h1 tag"),
-        React.createElement("h2", {}, "I am h2 tag")
+        React.createElement("h2", {}, "I am h2 tag woah")
     ]),    
 ]);
 
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # namaste-React
 Learning React and doing some cool projects
+
+
+# Parcel
+- Dev-build
+- Local Server
+- Auto refresh (HMR) - Hot Module Replacement, 
+ by using file watching algo written inn C++
+- Bundles
+- Compressing
+- Cache
+- Image Optimization
+- Prod Build & Dev Build
+- Tree Shaking - Remove unused codes
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - Helps in running the hosted app in older browser versions as well (using browsersList)
+- Diagnostics - Good Error Handling
+
+
