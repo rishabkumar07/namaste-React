0e7caafe29f811fb95e11f8f49122df34ecf23bc
RISHAB GUPTA <rishabkumar0703@gmail.com>
2024-02-23 20:42:34 +0530

    ep 8 class based comp lifecycle

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,32 @@
 import User from "./User";
 import UserClass from "./UserClass";
 import User from "./User";
+import React from "react";
 
-const About = () => {
-    return (
+class About extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+      return (
         <div>
-            <h1>About Us</h1>
-            <h2>Raj Restaurant in Jh</h2>
-            {/* <User name = {"Raj (function)"} location= {"Bokaro (function)"}/> */}
-            <UserClass  name = {"Raj (class)"} location= {"Bokaro (class)"}/>
+          <h1>About Us</h1>
+          <h2>Raj Restaurant in Jh</h2>
+          <UserClass />
         </div>
-    );
-};
+      );
+    }
+}
+
+// const About = () => {
+//     return (
+//         <div>
+//             <h1>About Us</h1>
+//             <h2>Raj Restaurant in Jh</h2>
+//             {/* <User name = {"Raj (function)"} location= {"Bokaro (function)"}/> */}
+//             <UserClass  name = {"Raj (class)"} location= {"Bokaro (class)"}/>
+//         </div>
+//     );
+// };
 
 export default About;
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -4,23 +4,35 @@ class UserClass extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      count1: 0,
-      count2 : 2 
+      userInfo : {
+        name: "Test name",
+        location: "India"
+      }
     }
   }
 
+  async componentDidMount() {
+    const data = await fetch("https://api.github.com/users/rishabkumar07");
+    const jsonData = await data.json();
+    console.log(jsonData);
+    this.setState({
+      userInfo : jsonData
+    });
+  }
+
+  componentDidUpdate() {
+    console.log("Update is called");
+  }
+
+  componentWillUnmount() {
+    console.log("Called before component is unmounted");
+  }
+
   render () {
-    const {name, location} = this.props;
-    const {count1} = this.state;
+    const {name, location} = this.state.userInfo;
 
     return (
       <div className="user-card">
-        <h1>Count : {count1}</h1>
-        <button onClick= {()=> {
-          this.setState({
-            count1 : this.state.count1 + 1,
-          })
-        }}>Counter</button>
         <h2>Name: {name}</h2>
         <h3>Location: {location}</h3>
         <h4>Mail: raj@march7</h4>
